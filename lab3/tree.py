669e32ede79a31af037c63ecabf09fa988fbdb32
LeoonKameleon <kacperwachala0@gmail.com>
2025-11-27 20:06:28 +0100

    lab3 structure

--- lab3/tree.py
+++ lab3/tree.py
@@ -1,6 +1,42 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from matrix import Matrix
 import lab3
+
+def power_iteration(M, max_iter=1000, eps=1e-8):
+    v = np.random.rand(M.shape[1])
+    v /= np.linalg.norm(v)
+    prev = np.empty(M.shape[1])
+    for _ in range(max_iter):
+        prev[:] = v
+        v = np.dot(M, v)
+        v /= np.linalg.norm(v)
+        if np.allclose(v, prev, atol=eps):
+            break
+    eigval = np.dot(v, np.dot(M, v)) / np.dot(v, v)
+    return v, eigval
+
+def truncated_svd(M, r, eps):
+    U = np.zeros([M.shape[1], r])
+    D = np.zeros(r)
+    V = np.zeros([M.shape[1], r])
+
+    A = M.T @ M
+    for rank in range(r):
+        eigvec, eigval = power_iteration(A)
+        if eigval < eps:
+            break
+
+        singular = np.sqrt(eigval)
+        D[rank] = singular
+
+        V[:, rank] = eigvec
+        U[:, rank] = A @ eigvec / eigval
+
+        A -= singular * eigvec @ eigvec.T
+
+    return U, D, V.T
+
 
 class Node:
     def __init__(self, rank, size, U=None, V=None, D=None):
@@ -55,3 +91,17 @@ def rebuild_matrix(node):
     bottom_right = rebuild_matrix(node.children[3])
     return np.vstack((np.hstack((top_left, top_right)),
                       np.hstack((bottom_left, bottom_right))))
+
+
+def test(img):
+    r = img[:, :, 0]
+    g = img[:, :, 1]
+    b = img[:, :, 2]
+
+    plt.subplot(1, 4, 1).imshow(img)
+    plt.subplot(1, 4, 2).imshow(r)
+    plt.subplot(1, 4, 3).imshow(r)
+    plt.subplot(1, 4, 4).imshow(r)
+
+
+test()
