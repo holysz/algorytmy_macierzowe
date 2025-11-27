2a50ef5ac9a2b03a57a136a79aaed7b1404eea44
holysz <szymonholysz@student.agh.edu.pl>
2025-11-27 19:12:16 +0100

    create tree

--- lab3/tree.py
+++ lab3/tree.py
@@ -1,49 +1,56 @@
 import numpy as np
+from matrix import Matrix
 
 class Node:
-    def __init__(self, rank, size, sing_vals=None, U=None, V=None):
+    def __init__(self, rank, size, U=None, V=None, D=None):
         self.rank = rank
         self.size = size
-        self.sing_vals = sing_vals
         self.U = U
         self.V = V
+        self.D = D
         self.children = []
 
-def svd_decomposition(A, k, eps):
-    #TODO
-    pass
-
-def svd_compression(A, max_rank, eps):
-    #TODO
-    pass
-
-def error(A, U, D, V):
-    #TODO
-    pass
-
-def create_tree(A, max_rank, eps=1e-10, min_size=2):
-    U, D, V = svd_compression(A, max_rank, eps)
-
-    root = Node(0, A.shape)
-
-    if min(A.shape) <= min_size or error(A, U, D, V) <= eps:
-        root.U = U
-        root.V = V
-        return root
+def truncatedSVD(A, r, eps):
+    raise "UNIMPLEMENTED"
     
-    mid_row = A.shape[0] // 2
-    mid_col = A.shape[1] // 2
+def CompressMatrix(A, U, D, V, r):
+    raise "UNIMPLEMENTED"
 
-    submatrices = [
-        A[:mid_row, :mid_col],
-        A[:mid_row, mid_col:],
-        A[mid_row:, :mid_col],
-        A[mid_row:, mid_col:]
-    ]
+def rebuild_matrix(node):
+    if not node.children:
+        return node.U @ node.S @ node.V
+    
+    return np.vstack((np.hstack((rebuild_matrix(node.children[0]), rebuild_matrix(node.children[1])),
+                      np.hstack((rebuild_matrix(node.children[2]), rebuild_matrix(node.children[3]))))))
 
-    for subm in submatrices:
-        if subm.size > 0:
-            root.children.append(create_tree(subm, max_rank, eps, min_size))
+def error(original, root):
+    compressed = rebuild_matrix(root)
+    return np.linalg.norm(original - compressed, ord='fro') / np.linalg.norm(original, ord='fro')
+
+def error(original, U, S, V):
+    compressed = U @ S @ V
+    return np.linalg.norm(original - compressed, ord='fro') / np.linalg.norm(original, ord='fro')
+
+def create_tree(A, rank, eps=1e-10, min_size=2):
+    U, D, V = truncatedSVD(A, rank + 1, eps)
+
+    if min(A.shape) <= min_size or D[rank] <= eps:
+        root = CompressMatrix(A, U, D, V, rank)
+    else:
+        root = Node(0, A.shape)
+        mid_row = A.shape[0] // 2
+        mid_col = A.shape[1] // 2
+
+        submatrices = [
+            A[:mid_row, :mid_col],
+            A[:mid_row, mid_col:],
+            A[mid_row:, :mid_col],
+            A[mid_row:, mid_col:]
+        ]
+
+        for subm in submatrices:
+            if subm.size > 0:
+                root.children.append(create_tree(subm, rank, eps, min_size))
 
     return root
 
